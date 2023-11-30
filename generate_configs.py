05cd049fc5b8ac3f1f19ed811a330b62d9e39f7d
tanushreebanerjee <tb21@princeton.edu>
2023-11-30 18:02:22 -0500

    updates

---
+++ generate_configs.py
@@ -0,0 +1,40 @@
+import yaml
+from itertools import product
+
+# Define model names, error types, and degrees
+models = ["Xwin-LM-7B-v0.1", "LLAMA-2-7B-Chat", "MPT-7B-chat", "vicuna-7b-v1.3"]
+error_types = ['speaker_identification', 'speech_recognition']
+degrees = [0.1, 0.2, 0.3]  # Adjust as needed
+
+# Define other parameters for experiments
+sample_sizes = [100, 200, 300]  # Adjust as needed
+corpora = {'ISCI': 'data/raw/ISCI_corpus', 'AMI': 'data/raw/AMI_corpus'}
+
+# Generate all combinations of parameters
+experiments = list(product(models, error_types, degrees, sample_sizes, corpora.items()))
+
+# Function to generate a configuration file for each experiment
+def generate_config_file(experiment):
+    model, error_type, degree, sample_size, (corpus_name, corpus_path) = experiment
+    config = {
+        'experiment_name': f'{model}_{error_type}_error_{degree}_sample_{sample_size}_{corpus_name}',
+        'model_name': model,
+        'error_types': [error_type],
+        'degree_of_error': degree,
+        'sample_size': sample_size,
+        'corpus_name': corpus_name,
+        'corpus_path': corpus_path,
+        # Add other experiment-specific parameters as needed
+    }
+
+    # Write the configuration to a YAML file
+    with open(f'configs/{config["experiment_name"]}.yaml', 'w') as file:
+        yaml.dump(config, file)
+
+# Create a directory for configuration files
+import os
+os.makedirs('configs', exist_ok=True)
+
+# Generate configuration files for all experiments
+for experiment in experiments:
+    generate_config_file(experiment)
--- main.py
+++ main.py
@@ -1,29 +1,51 @@
-# Main Script (main.py):
 import os
-from src.data_preparation import DataPreparation
-from src.error_injection import ErrorInjection
-from src.summarization import Summarization
-from src.evaluation import Evaluation
+import yaml
+from data_preparation import DataPreparation
+from error_injection import ErrorInjection
+from summarization import Summarization
+from evaluation import Evaluation
 
-# Main execution flow
-corpus_path = os.path.join(os.getcwd(), 'data/raw/AMI_corpus')
-data_prep = DataPreparation(corpus_path)
-transcripts_df = data_prep.load_transcripts()
-baseline_transcripts = data_prep.create_baseline_transcripts(transcripts_df)
+# Function to load experiment configurations from YAML files
+def load_experiment_config(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    return config
 
-error_types = ['speaker_identification', 'speech_recognition']
-error_injector = ErrorInjection(error_types)
-degree = 0.1 # 10% of the words in the transcript will be erroneous
-transcripts_with_errors = [error_injector.inject_errors(t, degree) for t in baseline_transcripts]
+# Function to run an experiment based on the provided configuration
+def run_experiment(config):
+    # Data Preparation
+    data_prep = DataPreparation(corpus_path=config['corpus_path'])
+    transcripts_df = data_prep.load_transcripts()
+    baseline_transcripts_df = data_prep.create_baseline_transcripts(transcripts_df, sample_size=config['sample_size'])
 
-llm_model_name = "Xwin-LM-7B-v0.1" 
-summarizer = Summarization(llm_model_name)
-summaries = [summarizer.summarize(t) for t in transcripts_with_errors]
+    # Error Injection
+    error_injector = ErrorInjection(config['error_types'])
+    transcripts_with_errors = [error_injector.inject_errors(t, degree=config['degree_of_error']) for t in baseline_transcripts_df['transcript']]
 
-# Assuming 'annotations' is a column in your DataFrame
-annotations = transcripts_df['annotations']
+    # Summarization
+    summarizer = Summarization(model_name=config['model_name'])
+    summaries = [summarizer.summarize(t) for t in transcripts_with_errors]
 
-evaluation = Evaluation()
-for i in range(len(transcripts_with_errors)):
-    rouge_scores = evaluation.calculate_rouge(annotations[i], summaries[i])
-    print(f"ROUGE Scores for Example {i}: {rouge_scores}")
+    # Evaluation
+    evaluation = Evaluation()
+    references = baseline_transcripts_df['reference_summary']
+
+    # Calculate and store ROUGE scores
+    rouge_scores = []
+    for i in range(len(transcripts_with_errors)):
+        rouge_scores.append(evaluation.calculate_rouge(references[i], summaries[i]))
+
+    # Store results (adjust the storage method based on your needs)
+    result_path = f'results/{config["experiment_name"]}_results.yaml'
+    with open(result_path, 'w') as result_file:
+        yaml.dump({'ROUGE_scores': rouge_scores}, result_file)
+
+# Create a directory for result files
+os.makedirs('results', exist_ok=True)
+
+# Run experiments for all generated configurations
+config_files = [f for f in os.listdir('configs') if f.endswith('.yaml')]
+for config_file in config_files:
+    config_path = os.path.join('configs', config_file)
+    config = load_experiment_config(config_path)
+    run_experiment(config)
