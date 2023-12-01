f0820e92bfbf920f8720ed82bdd6449eb2d4cf69
tanushreebanerjee <tb21@princeton.edu>
2023-11-30 21:32:44 -0500

    updateS

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 conda create -n cos514 python=3.9
-conda install -c anaconda pandas
-conda install -c conda-forge transformers
-pip install rouge datasets openai backoff soundfile
+conda install -c anaconda pandas=2.1.1
+conda install -c conda-forge transformers=4.31.0
+pip install rouge=1.0.1 datasets=2.14.7 openai=1.3.6 backoff=2.2.1 soundfile=0.12.1
