69fcc79c49333bf48b7c22414c1dd4e51626a973
tanushreebanerjee <tb21@princeton.edu>
2023-11-30 21:29:15 -0500

    updateS

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
-conda create -n cos514 python=3.9
-conda install -c anaconda pandas
-conda install -c conda-forge transformers
-pip install rouge datasets openai backoff soundfile
+conda create --name cos514 --file requirements.txt
+# conda create -n cos514 python=3.9
+# conda install -c anaconda pandas
+# conda install -c conda-forge transformers
+# pip install rouge datasets openai backoff soundfile
